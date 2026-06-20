c995343e18e5935bb9d01f3099cce949bc7e6ced
Loga Aswin <aswinramesh04@gmail.com>
2026-06-20 19:23:10 +0530

    fix

--- app/services/clip_service.py
+++ app/services/clip_service.py
@@ -1,23 +1,30 @@
-# app/services/clip_service.py
-
 from PIL import Image
 from transformers import CLIPProcessor, CLIPModel
 import torch
 
+
 class ClipService:
     def __init__(self):
-        self.model = CLIPModel.from_pretrained(
-            "openai/clip-vit-base-patch32"
-        )
-        self.processor = CLIPProcessor.from_pretrained(
-            "openai/clip-vit-base-patch32"
-        )
+        self.model = None
+        self.processor = None
+
+    def load(self):
+        if self.model is None:
+            self.model = CLIPModel.from_pretrained(
+                "openai/clip-vit-base-patch32"
+            )
+
+            self.processor = CLIPProcessor.from_pretrained(
+                "openai/clip-vit-base-patch32"
+            )
 
     def is_paddy_leaf(
         self,
         image_path: str,
         threshold: float = 0.60
     ) -> bool:
+
+        self.load()
 
         image = Image.open(image_path).convert("RGB")
 
